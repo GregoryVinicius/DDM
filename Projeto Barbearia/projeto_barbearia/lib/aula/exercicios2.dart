36ca5ddc6652db95c2d4d5ee754e35267514158d
GregoryVinicius <gregoryviniciuscla@gmail.com>
2024-02-29 20:48:28 -0300

    Desafio

--- Projeto Barbearia/projeto_barbearia/lib/aula/exercicios2.dart
+++ Projeto Barbearia/projeto_barbearia/lib/aula/exercicios2.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:io';
 
 void main(List<String> args) {
@@ -28,17 +29,19 @@ d) Faça 3 exemplos do seu projeto.
   while (nota1 < 0 || nota1 > 10) {
     print('nota não permitida.');
     print('Primeira nota: ');
+
     var entradaNota1 = stdin.readLineSync()!;
-    var nota1 = int.parse(entradaNota1);
+    nota1 = int.parse(entradaNota1);
   }
 
   print('Segunda nota: ');
   var entradaNota2 = stdin.readLineSync()!;
-  var nota2 = int.parse(entradaNota2);
+  nota2 = int.parse(entradaNota2);
 
     while (nota2 < 0 || nota2 > 10) {
     print('nota não permitida.');
     print('Segunda nota: ');
+
     var entradaNota2 = stdin.readLineSync()!;
     var nota2 = int.parse(entradaNota2);
   }
@@ -46,6 +49,8 @@ d) Faça 3 exemplos do seu projeto.
   var media = (nota1 + nota2) / 2;
   print('sua media foi $media');
 */
+
+/*
 // c) Com a média calculada, verifique se o aluno está aprovado (maior ou igual à 6).
 
   print('Primeira nota: ');
@@ -55,8 +60,9 @@ d) Faça 3 exemplos do seu projeto.
   while (nota1 < 0 || nota1 > 10) {
     print('nota não permitida.');
     print('Primeira nota: ');
+
     var entradaNota1 = stdin.readLineSync()!;
-    var nota1 = int.parse(entradaNota1);
+    nota1 = int.parse(entradaNota1);
   }
 
   print('Segunda nota: ');
@@ -66,8 +72,9 @@ d) Faça 3 exemplos do seu projeto.
   while (nota2 < 0 || nota2 > 10) {
     print('nota não permitida.');
     print('Segunda nota: ');
+
     var entradaNota2 = stdin.readLineSync()!;
-    var nota2 = int.parse(entradaNota2);
+    nota2 = int.parse(entradaNota2);
   }
 
   var media = (nota1 + nota2) / 2;
@@ -75,7 +82,81 @@ d) Faça 3 exemplos do seu projeto.
 
   if (media > 6.0) {
     print('Aprovado');
-  }else{
-    
+  } else {
+    print('Reprovado');
   }
+*/
+
+  // d) Faça 3 exemplos do seu projeto.
+/* */
+
+  // desafio fazer o exercicio 1 usando funções, funções com retorno, funções com parametro e retorno;
+
+  int verificarNota1(int nota1) {
+    while (nota1 < 0 || nota1 > 10) {
+      print('nota não permitida.');
+      print('Primeira nota: ');
+
+      var entradaNota1 = stdin.readLineSync()!;
+      nota1 = int.parse(entradaNota1);
+    }
+
+    return nota1;
+  }
+
+  int entradaNota1() {
+    print('Primeira nota: ');
+    var entradaNota1 = stdin.readLineSync()!;
+    var nota1 = int.parse(entradaNota1);
+
+    verificarNota1(nota1);
+
+    return nota1;
+  }
+
+  int verificarNota2(int nota2) {
+    while (nota2 < 0 || nota2 > 10) {
+      print('nota não permitida.');
+      print('Segunda nota: ');
+
+      var entradaNota2 = stdin.readLineSync()!;
+      nota2 = int.parse(entradaNota2);
+    }
+
+    return nota2;
+  }
+
+  int entradaNota2() {
+    print('Segunda nota: ');
+    var entradaNota1 = stdin.readLineSync()!;
+    var nota2 = int.parse(entradaNota1);
+
+    verificarNota2(nota2);
+
+    return nota2;
+  }
+
+  double mediaFunc(int nota1, int nota2) {
+    var media = (nota1 + nota2) / 2;
+
+    print('sua media foi $media');
+
+    return media;
+  }
+
+  void aprovadoReprovado(double media) {
+    if (media > 6.0) {
+      print('Aprovado');
+    } else {
+      print('Reprovado');
+    }
+  }
+
+  var nota1 = entradaNota1();
+
+  var nota2 = entradaNota2();
+
+  var media = mediaFunc(nota1, nota2);
+
+  aprovadoReprovado(media);
 }
